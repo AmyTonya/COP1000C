4b43c3dbd2c36ac2d2dbbdfb892ee54aa6c39261
AmyTonya <159963233+AmyTonya@users.noreply.github.com>
2024-02-14 12:30:54 -0500

    Update rpgv1.py
    V1 group project

--- rpgv1.py
+++ rpgv1.py
@@ -1,1 +1,20 @@
+#Group 2 Project file name: rpgv1.py
+#Names: Amy Griesmeyer and Sean Wilkie
+#date: February 14, 2024
+#Role Playing game
 
+#define main
+def main():
+    #display an intro
+    print("Welcome to the Trashcan Panda's Labyrinth! Enjoy!")
+    #display menu and choices
+    print("Please choose from the following menu:")
+    print("1) See rules")
+    print("2) Play game")
+    print("3) Exit")
+
+    #skip a line
+    print()
+    #enter input for choice
+
+    
